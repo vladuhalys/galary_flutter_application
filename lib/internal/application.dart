3e9cd2bf6f169755dbbbb53f968ae65e2295b4b1
Lysenko Vladyslav <33177301+vladuhalys@users.noreply.github.com>
2023-05-03 22:27:59 +0300

    first commit

---
+++ lib/internal/application.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/screens/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
---
+++ lib/internal/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:galary_flutter_application/internal/application.dart';
+
+void main() {
+  runApp(const Application());
+}
